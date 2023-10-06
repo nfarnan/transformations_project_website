54c7d376ec36fce8bab0a80b85b5b7ab5b5582f5
zhoffman1 <zfh1@pitt.edu>
2023-10-05 20:28:07 -0400

    Basic usable CSS for header and navbar

--- Transformations-website/src/pages/Layout.jsx
+++ Transformations-website/src/pages/Layout.jsx
@@ -3,18 +3,21 @@ import { Outlet, Link } from "react-router-dom"
 function Layout() {
   return (
     <>
-      <nav>
-        <ul>
-          <li>
+      <div className="header">
+        <h1>Transformations</h1>
+      </div>
+      <nav className="navbar">
+        <ul className="navbar-list">
+          <li className="navbar-item">
             <Link to="/">Home</Link>
           </li>
-          <li>
+          <li className="navbar-item">
             <Link to="/map">Map</Link>
           </li>
-          <li>
+          <li className="navbar-item">
             <Link to="/Timeline">Timeline</Link>
           </li>
-          <li>
+          <li className="navbar-item">
             <Link to="/Media">Media</Link>
           </li>
         </ul>
