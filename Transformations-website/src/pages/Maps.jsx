38a6ea228545d7767020bff61bd4d5042452265f
pasiphique <136754111+Pasiphique@users.noreply.github.com>
2023-10-06 20:19:46 -0400

    added map to Maps.jsx

--- Transformations-website/src/pages/Maps.jsx
+++ Transformations-website/src/pages/Maps.jsx
@@ -1,7 +1,15 @@
+import { MapContainer, TileLayer} from 'react-leaflet'
+import '../App.css' 
 function Map() { 
     return ( 
        <div> 
-            <h1> This is the Map page </h1> 
+            <h1> This is the Map page </h1>
+             <MapContainer center={[40.440624,-79.995888]} zoom={14}>
+                <TileLayer
+                  attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+                  url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+                />
+              </MapContainer> 
       </div> 
     ); 
 } 
