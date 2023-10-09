65f7bb031c8dd86e07dcff889f8ff1af416f8549
zhoffman1 <zfh1@pitt.edu>
2023-10-09 15:46:22 -0400

    Add HTML-ONLY non-functioning, non-React audio players

--- Transformations-website/src/pages/Media.jsx
+++ Transformations-website/src/pages/Media.jsx
@@ -2,6 +2,17 @@ function Media() {
     return ( 
        <div> 
             <h1> This is the Media page </h1> 
+            <audio controls autoplay muted>
+              <source src="audio-files/woods-noises.wav" type="audio/wav"></source>
+            </audio>
+            <div></div>
+            <audio controls autoplay muted>
+              <source src="audio-files/woods-noises.wav" type="audio/wav"></source>
+            </audio>
+            <div></div>
+            <audio controls autoplay muted>
+              <source src="audio-files/woods-noises.wav" type="audio/wav"></source>
+            </audio>
       </div> 
     ); 
 } 
