77e9a8f348f650bb8873b2b9daae5b2c9ef267b9
Pasiphique <pasiphiqueoswald@gmail.com>
2023-11-15 13:19:22 -0500

    fixed double legend render for strictmode only

--- Transformations-website/src/pages/Maps.jsx
+++ Transformations-website/src/pages/Maps.jsx
@@ -1,34 +1,34 @@
 import {MapContainer, TileLayer, LayersControl,GeoJSON} from 'react-leaflet'
+import {useState } from 'react'
 import 'leaflet/dist/leaflet.css'
 import 'leaflet/dist/leaflet'
-import data from '../pages/mapData/pitt.json'
-import {useState } from 'react'
-import geo2020 from '../pages/mapData/2020_zipCode.json'
-import geo1980 from '../pages/mapData/geo1980.json'
-import map1980 from '../pages/mapData/1980.json'
+import zipcodeData from '../pages/mapData/Zip and Tract/pitt.json'
+import tractData from '../pages/mapData//Zip and Tract/1980.json'
+import geo2020 from '../pages/mapData/Geojson/2020_zipCode.json'
+import geo1980 from '../pages/mapData/Geojson/geo1980.json'
 import Legend from '../pages/mapData/Legend'
 import '../App.css'
-import Controls  from './mapData/Control'
 const center = [40.44, -79.99]
 export default function Map() { 
 
     const [map, setMap] = useState(null)
     const table = []
     
-    for(let key in data){
+    // add 2022 map data with layer control
+    for(let key in zipcodeData){
       table.push(<LayersControl.BaseLayer key = {key} checked name = {key}>
-          <GeoJSON  style={{color:"black"}}  data= {pittZips()} onEachFeature= {onEachZipCodeCloser(data[key])}/>         
+          <GeoJSON  style={{color:"black"}}  data= {pittZips()} onEachFeature= {onEachZipCodeCloser(zipcodeData[key])}/>         
       </LayersControl.BaseLayer> )   
     }
-    
+     
     // add 1980 map data with a layer control
-    for(let key in map1980){
+    for(let key in tractData){
       table.push(<LayersControl.BaseLayer key = {key} checked={false} name = {key}> 
-        < GeoJSON  data= {geo1980} onEachFeature={onEachTract(map1980[key])}/>          
+        < GeoJSON  data= {geo1980} onEachFeature={onEachTract(tractData[key])}/>          
       </LayersControl.BaseLayer> )  
     }
     return ( 
-      <MapContainer center={center} zoom={11} scrollWheelZoom={false} whenReady={(map) =>{setMap(map.target)}}  >
+      <MapContainer center={center} zoom={11} scrollWheelZoom={false}  whenReady={(map) =>{setMap(map.target)}}  >
         <TileLayer
           attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
           url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
@@ -41,7 +41,6 @@ export default function Map() {
     )
   } 
 
- 
 function pittZips(){
     const zips = [15201,15122,15202,15203,15204,15205,15206,15207,15208,15209,15210,15211,15212,15213,15214,15215,15216,15217,15218,15219,15220,15221,15222,15223,15224,15225,15226,15227,15228,15229,15230,15231,15232,15233,15234,15235,15236,15237,15238,15239,15240,15241,15242,15243,15244,15250,15251,15260,15264,15272,15274,15275,15276,15290,15295]
     const pittsburghData = []
@@ -53,7 +52,8 @@ function pittZips(){
     }
     return pittsburghData
   }
-  
+
+// adds the colors based on population for each zip code region
 function onEachZipCodeCloser(data){
   return function onEachFeature(feature,layer){
     Object.entries(data).forEach((entry) => {
@@ -72,13 +72,13 @@ function onEachZipCodeCloser(data){
     });
   }
 }
+// adds the colors based on population for each tract region
 function onEachTract(data){
   return function onEachFeature(feature,layer){
     Object.entries(data).forEach((entry) => {
       let [tract, pop] = entry;
       pop = pop.toString().replaceAll(',','')
       if(feature.properties.tract == tract){
-        console.log(layer)
         layer.setStyle({
          color: "black",
          fill:true,
