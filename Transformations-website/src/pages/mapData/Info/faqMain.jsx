df5a1d1165df2d4626b136b2a128fbde62d9a7a4
Pasiphique <pasiphiqueoswald@gmail.com>
2023-11-27 12:37:29 -0500

    changed zipcode/census qquestions

--- Transformations-website/src/pages/mapData/Info/faqMain.jsx
+++ Transformations-website/src/pages/mapData/Info/faqMain.jsx
@@ -14,14 +14,14 @@ export default function faqMain(){
         <main>
         <section class="faq-container">
             <div class="faq-one">
-                <h1 class="faq-page" onClick={(e) => onEvent(e)}>What is a Zip Code?</h1>
+                <h1 class="faq-page" onClick={(e) => onEvent(e)}>What are Zip Codes?</h1>
                 <div class="faq-body">
                     <p>A system of code numbers assigned by the postal service to be used as part of the mailing address: each code designates a delivery area.</p>
                 </div>
             </div>
             <hr class="hr-line"/>
             <div class="faq-two">
-                <h1 class="faq-page" onClick={(e) => onEvent(e)}>What is a Census Tract?</h1>
+                <h1 class="faq-page" onClick={(e) => onEvent(e)}>What are Census Tracts?</h1>
                 <div class="faq-body">
                     <p>A census tract is a geographic region defined for the purpose of taking a census. It is a small relatively permanent subdivions of a county.</p>
                 </div>
